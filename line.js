b185d085a3be4a352bb404c70b3ab37c7edcacc4
jvahle <48984317+webflowjvahle@users.noreply.github.com>
2022-12-22 19:25:02 +0100

    Line Animaiton Portfolio

--- line.js
+++ line.js
@@ -1,1 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Split text and GSAP Animation
+  let typeSplit;
 
+  // Split the text up
+  function runSplit() {
+    typeSplit = new SplitType(".split-lines", {
+      types: "lines, words",
+    });
+    $(".line").append("<div class='line-mask'></div>");
+    createAnimation();
+  }
+  runSplit();
+
+  function createAnimation() {
+    $(".line").each(function (index) {
+      let tl = gsap.timeline();
+      tl.to($(this).find(".line-mask"), {
+        width: "0%",
+        duration: 5,
+      });
+    });
+  }
+});
