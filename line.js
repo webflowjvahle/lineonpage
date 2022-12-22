1409cd71a96b6752f1670e633a614bf6c6c3624e
jvahle <48984317+webflowjvahle@users.noreply.github.com>
2022-12-22 20:11:51 +0100

    Waiting for DOM Loaded

--- line.js
+++ line.js
@@ -1,22 +1,24 @@
-// Split text and GSAP Animation
-let typeSplit;
+document.addEventListener("DOMContentLoaded", () => {
+  // Split text and GSAP Animation
+  let typeSplit;
 
-// Split the text up
-function runSplit() {
-  typeSplit = new SplitType(".split-lines", {
-    types: "lines, words",
-  });
-  $(".line").append("<div class='line-mask'></div>");
-  createAnimation();
-}
-runSplit();
-
-function createAnimation() {
-  $(".line").each(function (index) {
-    let tl = gsap.timeline();
-    tl.to($(this).find(".line-mask"), {
-      width: "0%",
-      duration: 5,
+  // Split the text up
+  function runSplit() {
+    typeSplit = new SplitType(".split-lines", {
+      types: "lines, words",
     });
-  });
-}
+    $(".line").append("<div class='line-mask'></div>");
+    createAnimation();
+  }
+  runSplit();
+
+  function createAnimation() {
+    $(".line").each(function (index) {
+      let tl = gsap.timeline();
+      tl.to($(this).find(".line-mask"), {
+        width: "0%",
+        duration: 5,
+      });
+    });
+  }
+});
